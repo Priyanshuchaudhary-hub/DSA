9c01e60a73759433eabd3fefd8b11e1ab9da85a0
Priyanshu Bharangar <229632734+Priyanshuchaudhary-hub@users.noreply.github.com>
2026-08-01 23:06:37 +0530

    Add files via upload

---
+++ Reverse_Star.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Reverse_Star {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5 - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+    }
+}

7fcf70f927f6f53fa10297034ad72f91239fe9f5
Priyanshu Bharangar <229632734+Priyanshuchaudhary-hub@users.noreply.github.com>
2026-08-02 23:32:20 +0530

    Add files via upload

---
+++ Square_Of_A_Number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Square_Of_A_Number {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int n = sc.nextInt();
+        int square = n*n;
+        System.out.println(square);
+    }
+}

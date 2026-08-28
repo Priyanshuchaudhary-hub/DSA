73ede8244ea7baaa23bcf0830e1a0941f1a4ddfc
Priyanshu Bharangar <229632734+Priyanshuchaudhary-hub@users.noreply.github.com>
2026-08-28 16:53:05 +0530

    Time: 1545 ms (13.91%) | Memory: 48.6 MB (79.75%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,6 +1,6 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int max = Integer.MIN_VALUE;
+     //   int max = Integer.MIN_VALUE;
         int count = 0;
         for(int i = 0;i<nums.length;i++){
             int sum = 0;

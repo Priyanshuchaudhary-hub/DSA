2155415428413f9564a729caeffa8913385ec198
Priyanshu Bharangar <229632734+Priyanshuchaudhary-hub@users.noreply.github.com>
2026-08-28 17:06:03 +0530

    Time: 15 ms (18.73%) | Memory: 47.5 MB (10.81%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,8 @@
+class Solution {
+  public boolean isPalindrome(String s) {
+   String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+   StringBuilder sb = new StringBuilder(cleaned);
+   String reversed = sb.reverse().toString();
+   return cleaned.equals(reversed);
+    }
+}
